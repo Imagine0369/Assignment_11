9c8287a891021cf38c01dd5823fa098781bf85ab
sethpolevoi <74836049+sethpolevoi@users.noreply.github.com>
2021-12-05 18:25:17 -0700

    try catch bufferedReader

--- CS2420-Assign11/src/comprehensive/RandomPhraseGenerator.java
+++ CS2420-Assign11/src/comprehensive/RandomPhraseGenerator.java
@@ -1,9 +1,18 @@
 package comprehensive;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
 public class RandomPhraseGenerator {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		try {
+			BufferedReader bf = new BufferedReader( new FileReader( args[0] ) );
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
 	}
 
