26935dad81b18348ff4c1db7889c6adfa01966ca
sethpolevoi <74836049+sethpolevoi@users.noreply.github.com>
2021-12-05 20:15:13 -0700

    added NonTerminal and Terminal classes

---
+++ CS2420-Assign11/src/comprehensive/NonTerminal.java
@@ -0,0 +1,13 @@
+package comprehensive;
+
+import java.util.ArrayList;
+
+public class NonTerminal {
+	
+	private ArrayList<Terminal> termArray;
+	
+	public void add(Terminal term) {
+		
+	}
+
+}
--- CS2420-Assign11/src/comprehensive/RandomPhraseGenerator.java
+++ CS2420-Assign11/src/comprehensive/RandomPhraseGenerator.java
@@ -1,19 +1,51 @@
 package comprehensive;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
 
 public class RandomPhraseGenerator {
+	
+	private HashMap<String, NonTerminal> nonTermHM;
 
 	public static void main(String[] args) {
+		//read in input stuff
 		try {
-			BufferedReader bf = new BufferedReader( new FileReader( args[0] ) );
-		} catch (FileNotFoundException e) {
+			readInInput("src/comprehensive/"+args[0]);
+		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 
+	}
+	
+	public static void readInInput(String fileLocation) throws IOException {
+		File inputFile = new File(fileLocation);
+		BufferedReader bf;
+		try {
+			bf = new BufferedReader( new FileReader( inputFile ) );
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+			bf = null;
+		}
+		String line;
+		while ((line = bf.readLine()) != null) {
+			boolean inputBegin = line.equals("{");
+			if( inputBegin ) {
+				String nonTerminalName = bf.readLine();
+//				nonTermHM.put(nonTerminalName,8);
+				String innerLine;
+				while ((innerLine = bf.readLine()) != "}") {
+					
+				}
+			}
+			System.out.println(line);
+//	        content.append(line);
+//	        content.append(System.lineSeparator());
+	    }
 	}
 
 }
